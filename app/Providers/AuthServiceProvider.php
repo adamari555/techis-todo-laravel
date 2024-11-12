576321d00636c3170f4cdc1313f9c84be748d921
adamari555 <yusumi2428@gmail.com>
2024-11-12 10:38:25 +0900

    first commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,8 +13,9 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
-    ];
+    // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    'App\Models\Task' => 'App\Policies\TaskPolicy',
+   ];
 
     /**
      * Register any authentication / authorization services.
